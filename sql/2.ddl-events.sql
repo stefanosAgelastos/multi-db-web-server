d359ae10cad9fc1866b36f3442f743d864661c84
Stefanos <sagelastos@gmail.com>
2021-10-07 10:33:31 +0200

    fix event to get today's time at 12:00

--- sql/2.ddl-events.sql
+++ sql/2.ddl-events.sql
@@ -5,5 +5,5 @@ DROP EVENT IF EXISTS deleteCodesDaily;
 
 CREATE EVENT deleteCodesDaily
 ON SCHEDULE EVERY 1 DAY
-        STARTS CURRENT_TIMESTAMP + INTERVAL 1 DAY
+        starts DATE_FORMAT(CURRENT_TIMESTAMP, '%Y-%m-%d 12:00:00')
 DO DELETE FROM `rollcall_db`.`code`;
