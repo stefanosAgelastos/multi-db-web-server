674816f9f3c5f1a8a8393837425786aa158a7123
Stefanos <sagelastos@gmail.com>
2021-11-04 12:52:07 +0100

    (MF-3) added console log

--- tests/globalTeardown.js
+++ tests/globalTeardown.js
@@ -1,4 +1,5 @@
 module.exports = async function () {
+    console.log("Tear down after tests");
     // we do not need to close the DB
     // await global.__DB__.close();
     // But we can use this function whenever we run some cleanup after finishing the tests
