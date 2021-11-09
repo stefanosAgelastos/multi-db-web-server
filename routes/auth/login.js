c120dff348cc3ce2eb37ba42d85898f6354a03f9
Zamanien <tari0048@stud.kea.dk>
2021-11-09 16:37:04 +0100

    Register teacher is working. Login to-be-implemented in next commit

--- routes/auth/login.js
+++ routes/auth/login.js
@@ -3,12 +3,36 @@ const router = express.Router();
 
 //Path to frontend folder. __dirname alone will only point to current path and not the correct one
 const path = require('path');
+const db = require('../../startup/db.mysql');
 const frontendPath = path.resolve (__dirname, '../../frontend/');
 router.use(express.static(frontendPath));
 
 
 router.get('/login', (req, res) => {
     return res.sendFile(frontendPath + '/login/login.html');
+});
+
+router.post('/login', async (req, res) => {
+    try{
+
+        //initialize variables with value from .body.username and password
+        const email = req.body.email;
+        const plainPassword = req.body.password;
+
+        const userInfo = await db.sequelize.models.teachers.findOne({ where: { email }});
+        console.log(userInfo[0][0])
+        /*
+        if (userInfo[0][0] === undefined || userInfo[0][0].length === 0) {
+            res.status(404).send(`User: ${username} not found!`);
+            return res.redirect('/login');
+
+        } else if (await bcrypt.compare(plainPassword, userInfo[0][0].password)) {
+            console.log(userInfo);
+        }
+        */
+    } catch(error) {
+
+    }
 });
 
 module.exports = router;
--- routes/auth/register.js
+++ routes/auth/register.js
@@ -1,64 +1,52 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
-const Teacher = require('../../models/teachers');
+const db = require("../../startup/db.mysql");
 
-//Path to frontend folder. __dirname alone will only point to current path and not the correct one
+
+//Modified Path
 const path = require('path');
-const frontendPath = path.resolve (__dirname, '../../frontend/');
+const frontendPath = path.resolve(__dirname, '../../frontend/');
+//Middleware to parse incoming requests
+router.use(express.urlencoded({ extended: false }));
 
-router.use(express.urlencoded({extended: false}));
-
-//temp storing user info here - to be replacted with DB later
-//conswt teachers = [];
-
+//GET
 router.get('/register', (req, res) => {
     return res.sendFile(frontendPath + '/register/register.html');
 });
 
-const db = require("../../startup/db.mysql");
-
+//POST
 router.post('/register', async (req, res) => {
 
-    //const { username, password, email } = req.body;
-    //const newTeacher = new Teacher({ username, password, email });
 
-    //console.log(teachers);  
-    
     try {
-        await db.sequelize.models.teachers.create({
 
-            first_name: req.body.first_name,
-            last_name: req.body.last_name,
-            email: req.body.email,
-            password: req.body.password,
-            department_id: req.body.department_id
-        }).then(newTeacher => res.send(newTeacher));
+        const { first_name, last_name, email, department_id } = req.body;
+        const password = await bcrypt.hash(req.body.password, 10);
 
-        console.log(newTeacher);
-    }
-    catch (error) {
-        res.status(501).send(error);
-    }
+        const teacherAlreadyExists = await db.sequelize.models.teachers.findOne({ where: { email } })
+            .catch((error) => {
+                console.log(error);
+            });
 
-    //console.log(teachers);
-    
-    
-/*
-    try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
-        teachers.push({
-            id: Date.now().toString(),
-            email: req.body.email,
-            username: req.body.username,
-            password: hashedPassword
-        });
-        console.log(teachers);
-        return res.redirect('/login');
+        if (teacherAlreadyExists) {
+            res.json({ message: 'Teacher with that Email already exists' });
+        } else {
+            await db.sequelize.models.teachers.create({
+
+                first_name,
+                last_name,
+                email,
+                password,
+                department_id,
+
+            }).then(newTeacher => res.send(newTeacher));
+            
+
+        }return res.redirect('/login');
     } catch (error) {
         res.status(501).send(error);
     }
-*/
 });
 
 
