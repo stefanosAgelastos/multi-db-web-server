f933b6c0561d478d250848b1dec1935f659d7d9e
Stefanos <sagelastos@gmail.com>
2021-11-21 23:11:01 +0100

    (FM-16) fix typo

--- sql/1.ddl.sql
+++ sql/1.ddl.sql
@@ -48,7 +48,7 @@ CREATE TABLE IF NOT EXISTS `rollcall_db`.`teachers_subjects` (
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `rollcall_db`.`presence_key` (
 `presence_key_id` INT NOT NULL AUTO_INCREMENT UNIQUE,
--- Can be changed when we deceide to generate number or url as a presence_key  --
+-- Can be changed when we decide to generate number or url as a presence_key  --
 `actual_presence_key` varchar(255) UNIQUE NOT NULL,
 `semester` VARCHAR(5) NOT NULL,
 `current_dateTime` DATETIME NOT NULL,
