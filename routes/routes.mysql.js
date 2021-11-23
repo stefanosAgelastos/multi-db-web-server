b10d9a0e17cee9cf6fc69b51d7458aafd3aea1df
Stefanos <sagelastos@gmail.com>
2021-11-23 21:52:15 +0100

    (FM-6) rename presence key to passphrase at client side

--- routes/routes.mysql.js
+++ routes/routes.mysql.js
@@ -1,4 +1,4 @@
-const presenceKeyRouter = require('./router.presenceKey');
+const passphraseRouter = require('./router.passphrase');
 const healthCheckRouter = require('./router.healthCheck');
 const loginRoute = require('./router.login');
 const registerRoute = require('./router.register');
@@ -7,7 +7,7 @@ const teacherAPI = require('./router.teacherAPI');
 const baseRoute = '/sql';
 
 module.exports = function (app) {
-    app.use(baseRoute, presenceKeyRouter)
+    app.use(baseRoute, passphraseRouter)
     app.use(baseRoute, healthCheckRouter)
     app.use(baseRoute, loginRoute);
     app.use(baseRoute, registerRoute);
