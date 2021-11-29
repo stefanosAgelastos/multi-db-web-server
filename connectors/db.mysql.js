bef94a3b4163ee2325168dad9d631970d7531fbe
Stefanos Agelastos <sagelastos@gmail.com>
2021-11-29 15:14:47 +0100

    refactor env vars (#23)
    Wooow

--- connectors/db.mysql.js
+++ connectors/db.mysql.js
@@ -4,13 +4,13 @@ const { initModels } = require('../models/init-models.js')
 // define mysql database connection
 const sequelize = new Sequelize({
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: process.env.MYSQLDB_LOCAL_PORT,
     username: process.env.DB_USER,
     password: process.env.DB_SECRET,
-    database: process.env.DB_NAME,
+    database: process.env.MYSQLDB_DATABASE,
     dialect: 'mysql'
 });
- 
+
 
 // define sequelize models
 initModels(sequelize);
