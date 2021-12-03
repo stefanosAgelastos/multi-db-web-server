eb47553a446cb62a7086e62b585da7309311e576
arch0047 <48592482+arch0047@users.noreply.github.com>
2021-12-03 17:14:45 +0100

    mongo db student

--- mongo/studentModel.js
+++ mongo/studentModel.js
@@ -16,7 +16,8 @@ const studentSchema = new mongoose.Schema({
     
     user_name: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     password: {
         type: String,
