f0641d86e63040bab71b6ae1786fd8635458f45f
Stefanos <sagelastos@gmail.com>
2021-12-09 16:11:55 +0100

    undelete files from main

--- routes/routes.mysql.js
+++ routes/routes.mysql.js
@@ -1,12 +1,18 @@
 
+const passphraseRouter = require('./router.passphrase');
+const checkinRouter = require('./router.checkin');
 const healthCheckRouter = require('./router.healthCheck');
 const loginRoute = require('./router.login');
 const registerRoute = require('./router.register');
+const teacherAPI = require('./router.teacherAPI');
 
 const baseRoute = '/sql';
 
 module.exports = function (app) {
+    app.use(baseRoute, passphraseRouter)
+    app.use(baseRoute, checkinRouter)
     app.use(baseRoute, healthCheckRouter)
     app.use(baseRoute, loginRoute);
     app.use(baseRoute, registerRoute);
+    app.use(baseRoute + '/teachers', teacherAPI);
 }
---
+++ sql/2.ddl-events.sql
@@ -0,0 +1,9 @@
+-- DELETE ALL CODES ONCE A DAY --
+USE rollcall_db;
+
+DROP EVENT IF EXISTS deleteCodesDaily;
+
+CREATE EVENT deletePresenceKeyDaily
+ON SCHEDULE EVERY 1 DAY
+        starts DATE_FORMAT(CURRENT_TIMESTAMP, '%Y-%m-%d 14:15:00')
+DO DELETE FROM `rollcall_db`.`presence_key`;
