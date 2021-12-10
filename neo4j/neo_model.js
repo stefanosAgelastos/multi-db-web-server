a30c596042d9987babb94aa593757bf95ac11c8c
Zamanien <tari0048@stud.kea.dk>
2021-12-10 01:22:54 +0100

    Students doesnt work atm. Need to find a way to add the data efficiently (and more automated)

--- neo4j/neo_model.js
+++ neo4j/neo_model.js
@@ -1,4 +1,6 @@
 CREATE 
+
+// Subject nodes
 (python:Subject {name:'Python'}),
 (java:Subject {name:'Java'}),
 (advanced_python:Subject {name:'Advanced Python'}),
@@ -12,16 +14,42 @@ CREATE
 (javascript:Subject {name:'Javascript'}),
 (testing:Subject {name:'Testing'}),
 
+// student nodes
+(john:Student {name:'John', season: 'Summer'}),
+(gabriel:Student {name:'Gabriel', season: 'Summer'}),
+(henry:Student {name:'Henry', season: 'Summer'}),
+(ali:Student {name:'Ali', season: 'Summer'}),
+(archana:Student {name:'Archana', season: 'Summer'}),
+(stefanos:Student {name:'Stefanos', season: 'Summer'}),
+(muhammad:Student {name:'Muhammad', season: 'Summer'}),
+(rafael:Student {name:'Rafael', season: 'Summer'}),
+(ismail:Student {name:'Ismail', season: 'Summer'}),
+(rikke:Student {name:'Rikke', season: 'Summer'}),
+(catlyn:Student {name:'Catlyn', season: 'Summer'}),
+(regitze:Student {name:'Regitze', season: 'Spring'}),
+(daria:Student {name:'Daria', season: 'Spring'}),
+(dunya:Student {name:'Dunya', season: 'Spring'}),
+(sanna:Student {name:'Sanna', season: 'Spring'}),
+(sama:Student {name:'Sama', season: 'Spring'}),
+(daoud:Student {name:'Daoud', season: 'Spring'}),
+(fardin:Student {name:'Fardin', season: 'Spring'}),
+(farid:Student {name:'Farid', season: 'Spring'}),
+(faruq:Student {name:'Faruq', season: 'Spring'}),
+
+// department node
 (kea:Department {name: 'KEA Department'}),
 
+// Semester nodes
 (sd22spr: Semester {name: 'SD22 Spring'}),
 (sd22sum: Semester {name: 'SD22 Summer'}),
 
+// teacher nodes
 (tomas:Teacher {name: 'Tomas',email:'tomas@kea.dk'}),
 (jarl:Teacher {name: 'Jarl',email:'jarl@kea.dk'}),
 (asger:Teacher {name: 'Asger',email:'asbc@kea.dk'}),
 (andrea:Teacher {name: 'Andrea',email:'ande@kea.dk'}),
 
+// subject node and semester node relations
 (database)-[:TAUGHT_IN]->(sd22spr),
 (python)-[:TAUGHT_IN]->(sd22spr),
 (java)-[:TAUGHT_IN]->(sd22spr),
@@ -35,6 +63,7 @@ CREATE
 (javascript)-[:TAUGHT_IN]->(sd22sum),
 (testing)-[:TAUGHT_IN]->(sd22sum),
 
+// teacher nodes and subjects nodes relations
 (tomas)-[:TEACHES]->(database),
 (tomas)-[:TEACHES]->(python),
 (tomas)-[:TEACHES]->(java),
@@ -48,8 +77,38 @@ CREATE
 (andrea)-[:TEACHES]->(javascript),
 (andrea)-[:TEACHES]->(testing),
 
+// teacher nodes and department nodes
 (tomas)-[:BELONGS_TO]->(kea),
 (jarl)-[:BELONGS_TO]->(kea),
 (asger)-[:BELONGS_TO]->(kea),
 (andrea)-[:BELONGS_TO]->(kea)
 
+// (john)-[:ATTENDS]->(),
+// (gabriel)-[:ATTENDS]->(),
+// (henry)-[:ATTENDS]->(),
+// (ali)-[:ATTENDS]->(),
+// (archana)-[:ATTENDS]->(),
+// (stefanos)-[:ATTENDS]->(),
+// (muhammad)-[:ATTENDS]->(),
+// (rafael)-[:ATTENDS]->(),
+// (ismail)-[:ATTENDS]->(),
+// (rikke)-[:ATTENDS]->(),
+// (catlyn)-[:ATTENDS]->(),
+// (regitze)-[:ATTENDS]->(),
+// (daria)-[:ATTENDS]->(),
+// (dunya)-[:ATTENDS]->(),
+// (sanna)-[:ATTENDS]->(),
+// (sama)-[:ATTENDS]->(),
+// (daoud)-[:ATTENDS]->(),
+// (fardin)-[:ATTENDS]->(),
+// (farid)-[:ATTENDS]->(),
+// (faruq)-[:ATTENDS]->(),
+
+
+// MATCH (a:Student) 
+// WHERE a.season = 'Summer'
+// CREATE (a)-[:GOES_TO]->(sd22sum)
+
+// MATCH (b:Student)
+// WHERE b.season = 'Spring'
+// CREATE (b)-[:GOES_TO]->(sd22spr)
