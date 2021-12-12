a465570a42c11d5c021ca59e6fd1260801198c27
Zamanien <tari0048@stud.kea.dk>
2021-12-12 13:50:58 +0100

    minor changes

--- routes/routes.neo.js
+++ routes/routes.neo.js
@@ -1,6 +1,6 @@
 const teacher_crud = require('../routes/router.teacher.neo');
 
-const neoRoute = 'neo';
+const neoRoute = '/neo';
 
 module.exports = function (app) {
     app.use(neoRoute + '/teachers', teacher_crud);
