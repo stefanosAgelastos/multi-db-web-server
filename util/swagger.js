203485075ede260a447276f611cdc1ca9e5ae4aa
Stefanos <sagelastos@gmail.com>
2021-12-14 11:05:25 +0100

    update port

--- util/swagger.js
+++ util/swagger.js
@@ -5,7 +5,7 @@ const doc = {
     title: 'Rollcall API documentation',
     description: 'Description of the application endpoints',
   },
-  host: 'localhost:3000',
+  host: 'localhost:8080',
   schemes: ['http'],
 };
 
