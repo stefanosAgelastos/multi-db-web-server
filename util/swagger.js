1fe3783c33b10a94dfe559f4078791b5ef7d3b5e
Stefanos Agelastos <sagelastos@gmail.com>
2021-12-14 14:51:44 +0100

    Add Swagger to documentation (#31)
    * initial commit, working
    can use some refactoring
    
    * fixed healthcheck endpoint
    
    * update port

---
+++ util/swagger.js
@@ -0,0 +1,19 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'Rollcall API documentation',
+    description: 'Description of the application endpoints',
+  },
+  host: 'localhost:8080',
+  schemes: ['http'],
+};
+
+const outputFile = './docs/swagger-output.json';
+const endpointsFiles = ['./index.js'];
+
+/* NOTE: if you use the express Router, you must pass in the 
+   'endpointsFiles' only the root file where the route starts,
+   such as index.js, app.js, routes.js, ... */
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
