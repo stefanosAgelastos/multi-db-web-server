0c309fff30bbadc3c99a52790448e025ef401d06
Zamanien <tari0048@stud.kea.dk>
2021-12-14 17:10:11 +0100

    find all and find one teacher works now

--- routes/router.teacher.neo.js
+++ routes/router.teacher.neo.js
@@ -2,15 +2,7 @@ const express = require('express');
 const router = express.Router();
 const neo_db = require('../connectors/db.neo');
 
-function formatResponse(resultInput) {
-    const result = [];
-    if (resultInput.records.length > 0) {
-        resultInput.records.map(record => {
-            result.push(record._fields[0].properties)
-        });
-    }
-    return result;
-}
+
 
 //endpoint for find all teachers
 router.get('/teachers/', async (req, res) => {
@@ -26,59 +18,40 @@ router.get('/teachers/', async (req, res) => {
             
             res.status(200).json(teachers);
         } else {
-            res.status(400).send('Can not get Teachers')
+            res.status(400).send('No teachers found')
         }
 
     } finally {
         // await neo_db.session.close();
     }
-
-    
     
 });
 
 //endpoint for find one teacher
-router.get('/teacher/:id', async (req, res) => {
-    
-    const id  = req.params;
-    console.log(id)
-    const query = `MATCH (n:Teacher {_id : '${id}'}) RETURN n`;
-    
-    const temp_teacher = await neo_db.session.run(query, id);
-    console.log(temp_teacher)
+router.get('/teacher/:id', async (req, res) => {  
+
+    const id  = req.params.id;
+    const query = `MATCH (n : Teacher) WHERE id(n) = {id} RETURN n`;
+    const params = { id: parseInt(id) };
+    const temp_teacher = await neo_db.session.run(query, params);
+
     if (temp_teacher == null) {
         res.status(404).send('Teacher not found')
     } else {
         const teacher = formatResponse(temp_teacher);
         res.status(200).json(teacher);
-        console.log(temp_teacher)
     }
 });
 
-// router.post('/teachers/create', (req, res) => {
-//     // Create a person node
-//     function addPerson (tx, name) {
-//         return tx.run('CREATE (a:Person {name: $name})', { name: name })
-//   }
-  
-// }); 
-// router.get('/teacher/:id', async (req, res) => {
-    
-//     const { id } = req.params;
-//     console.log(id)
-    
-//     const query = 'MATCH (n:Teacher) WHERE ID(n) = 27 RETURN n.name, n.email';
-//     const idConvert = {id: parseInt(id) }
-//     const teacher_out = await neo_db.session.run(query, idConvert);
-//     if (teacher_out == null) {
-//         res.status(404).send('Teacher not found')
-//     } else {
-//         const teacher = formatResponse(teacher_out)
-//         console.log(teacher)
-//         res.status(200).json(teacher);
-//     }
-// });
 
-// await neo_db.driver.close();
+function formatResponse(resultInput) {
+    const result = [];
+    if (resultInput.records.length > 0) {
+        resultInput.records.map(record => {
+            result.push(record._fields[0].properties)
+        });
+    }
+    return result;
+}
 
 module.exports = router;
