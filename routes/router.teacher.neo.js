4b07f1949c003da51b0b3381a88cfa7c3d6f93b4
Zamanien <tari0048@stud.kea.dk>
2021-12-14 15:34:48 +0100

    temp find 1 teacher. Not working properly atm. Fixes on the way (i hope :)

--- routes/router.teacher.neo.js
+++ routes/router.teacher.neo.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const neo_db = require('../connectors/db.neo');
 
 //endpoint for find all teachers
-router.get('/teachers/all', async (req, res) => {
+router.get('/teachers/', async (req, res) => {
     //get all teachers
     try {
         all_teachers = await neo_db.session.run(
@@ -11,28 +11,62 @@ router.get('/teachers/all', async (req, res) => {
         )
 
         if (all_teachers != null) {
+            
+            console.log('find all test', all_teachers)
             res.status(200).json(all_teachers);
         } else {
             res.status(400).send('Can not get Teachers')
         }
 
     } finally {
-        await neo_db.session.close();
+        // await neo_db.session.close();
     }
 
-    await neo_db.driver.close();
+    
     
 });
 
 //endpoint for find one teacher
 router.get('/teacher/:id', async (req, res) => {
-    //get 1 teacher
-    const { id } = req.params;
-    //To-be-modified. Queries should not be hardcoded
-    const query = 'MATCH (n:Teachers {id: $id}) RETURN n limit 5';
-    const params = { id: parseInt(id) };
-    const teacherObject = await neo_db(query, params);
-    res.send(teacherObject);
+
+    console.log('test')
+    
+    const id = req.params;
+    
+    const query = 'MATCH (n:Teacher) WHERE ID(n) = $id RETURN n.name, n.email';
+    const teacher = await neo_db.session.run(query, id);
+    console.log(teacher)
+    if (teacher == null) {
+        res.status(404).send('Teacher not found')
+    } else {
+        res.status(200).json(teacher);
+    }
 });
+
+// router.post('/teachers/create', (req, res) => {
+//     // Create a person node
+//     function addPerson (tx, name) {
+//         return tx.run('CREATE (a:Person {name: $name})', { name: name })
+//   }
+  
+// }); 
+// router.get('/teacher/:id', async (req, res) => {
+
+//     console.log('test')
+    
+//     const id = req.params.id;
+//     one_teacher = await neo_db.session.run(
+//         'MATCH (n:Teachers {id: $id}) RETURN n');
+//     console.log(id)
+//     const teacherObject = await neo_db(one_teacher, id);
+
+//     if (!teacherObject) {
+//         res.statusCode(404).send('Teacher not found')
+//     } else {
+//         res.statusCode(200).json(teacherObject);
+//     }
+// });
+
+// await neo_db.driver.close();
 
 module.exports = router;
