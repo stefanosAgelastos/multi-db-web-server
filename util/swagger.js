8d1b98b72f9d371c80ceab424c01dbd64d6b88da
Stefanos <sagelastos@gmail.com>
2021-12-15 19:40:37 +0100

    added swagger development mode

--- util/swagger.js
+++ util/swagger.js
@@ -16,4 +16,7 @@ const endpointsFiles = ['./index.js'];
    'endpointsFiles' only the root file where the route starts,
    such as index.js, app.js, routes.js, ... */
 
-swaggerAutogen(outputFile, endpointsFiles, doc);
+swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
+  if (process.env.SWAGGER_DEV) { require('../index.js'); }
+  else return;
+});
