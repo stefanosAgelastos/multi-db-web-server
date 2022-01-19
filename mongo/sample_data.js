d13cf2bf5300abbbcfec3d0e1eab71f701690930
arch0047 <48592482+arch0047@users.noreply.github.com>
2022-01-19 07:40:54 +0100

    Mongo teacher (#35)
    * mongoTeachers collection is created and working
    
    * some more changes in mongo
    
    * teacher Sample_data added in the file
    
    * event name modified in event.sql file
    
    * update dependencies
    
    * add footer fragment for documentation
    
    * added swager annotations for mongo teacher CRUD
    
    Co-authored-by: Stefanos <sagelastos@gmail.com>

--- mongo/sample_data.js
+++ mongo/sample_data.js
@@ -59,3 +59,53 @@ db.students.insertMany([
         ]
     }
 ]);
+
+
+db.createCollection('teachers');
+
+db.teachers.insertMany([
+    {
+        "name": "Tomas P",
+        "user_name": "TOMP@kea.dk",
+        "password": "12345678",
+        "semester": "1",
+        "subjects": [
+            {
+                "subjects_name": "Database"
+            },
+            {
+                "subjects_name": "Fullstack"
+            }
+        ]
+    },
+    {
+        "name": "Andrea C",
+        "user_name": "ANDC@kea.dk",
+        "password": "12345678",
+        "semester": "1",
+        "subjects": [
+            {
+                "subjects_name": "DLS"
+            },
+            {
+                "subjects_name": "Design Patterns"
+            }
+        ]
+    }, {
+        "name": "Jarl Tuxen",
+        "user_name": "JART@kea.dk",
+        "password": "12345678",
+        "semester": "2",
+        "subjects": [
+            {
+                "subjects_name": "Python"
+            },
+            {
+                "subjects_name": "Design Pattern"
+            },
+            {
+                "subjects_name": "System Intregation"
+            }
+        ]
+    }
+]);
